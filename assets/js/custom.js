d052da85dffb7d799da4a29b0b7efd8950317902
AN23702 <161464637+AN23702@users.noreply.github.com>
2024-03-12 21:26:35 +0700

    b7

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,21 +1,26 @@
 $(document).ready(function(){
     // $(".owl-carousel").owlCarousel();
     $('.owl-carousel').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      responsive:{
-          0:{
-              items:1
+      loop:true, /*Vong lap*/
+      margin:10, /*Khoang cach giua cac item*/
+      nav:true, /*Thanh dieu huong, hai mui ten*/
+      dots:true,/*Dau cham dieu huong*/
+      autoplay: true, /*Tu dong chuyen slide*/
+      autoplayTimeout: 4000,
+
+      responsive:{ /*Quy dinh so luong san pham tren cac man hinh*/
+
+          0:{ 
+              items:1 /*0->599px thi hien 1 san pham*/
           },
           600:{
-              items:3
+              items:1 /*601->999px thi hien 3 san pham*/
           },
           1000:{
-              items:5
+              items:1
           }
       }
   })
-  
+
   });
 
